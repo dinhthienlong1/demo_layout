fd77e2e45aa29a642e8f6f4526b9efedbfe63fb5
dinh thien long dev <dinhthienlong1@gmail.com>
2020-09-06 20:10:20 +0700

    aaaaaa

--- demo1.php
+++ demo1.php
@@ -24,6 +24,10 @@
                 <h2>Sidebar</h2>
                 <b>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</b>
             </div>
+            <div class="sidebar">
+                <h2>Sidebar</h2>
+                <b>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</b>
+            </div>
         </div>
     </div>
 </body>
