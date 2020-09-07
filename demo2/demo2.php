c4f97d76b46e3bad2db1a864a6e6624a85455480
thien long <dinhthienlong1@gmail.com>
2020-09-07 18:53:47 +0700

    aaaa

---
+++ demo2/demo2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="demo2.css">
+</head>
+<body >
+    <div class="wrapper">
+        <div class="contact-form">
+            <div class="input-fields">
+              <input type="text" class="input" placeholder="name">
+              <input type="text" class="input" placeholder="email">
+              <input type="text" class="input" placeholder="phone">
+              <input type="text" class="input" placeholder="subject">
+              
+            </div>
+            <div>
+                <textarea placeholder="message" class="smg" ></textarea>
+                <div class="btn" >send</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
